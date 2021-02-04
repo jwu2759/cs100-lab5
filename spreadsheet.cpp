43b82c25ac04bc59e8c944c69e5340e24a5def26
ryanchangx <ryanchangx@gmail.com>
2021-02-03 20:29:29 -0800

    Add Select_And class

--- spreadsheet.cpp
+++ spreadsheet.cpp
@@ -42,6 +42,17 @@ int Spreadsheet::get_column_by_name(const std::string& name) const
     return -1;
 }
 
-void print_selection(std::ostream& out) const{
-	
+void Spreadsheet::print_selection(std::ostream& out) const{
+	//puts sets all of the 1s and 0s
+	for(int i = 0 ; i < data.size(); ++i){
+		select->select(this,i);
+	}
+	for(int i = 0 ; i < bools.size(); ++i){
+		if(!bools.at(i)){
+			for(int j = 0; j < data.at(i).size(); ++j){
+				out << data.at(i).at(j) << " ";
+			}
+			out << std::endl;
+		}
+	}
 }
