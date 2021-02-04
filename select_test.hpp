b8a3180fcfb7998024278a76b94ca22a7b0530dd
ryanchangx <ryanchangx@gmail.com>
2021-02-03 23:15:55 -0800

    Added working test for Select_And.

--- select_test.hpp
+++ select_test.hpp
@@ -18,11 +18,30 @@ TEST(SelectContains, SelectContainsStandard){
     sheet.add_row({"David","Dole","22","electrical engineering"});
     sheet.add_row({"Dominick","Dole","22","communications"});
     sheet.add_row({"George","Genius","9","astrophysics"});
-
     sheet.set_selection(new Select_Contains(&sheet, "First", "Joe"));
     
     sheet.print_selection(sstr);
     ASSERT_EQ("Joe Jackson 21 mathematics \n",sstr.str());
+}
+
+TEST(SelectAnd, SelectAndStandard){	
+    Spreadsheet sheet;
+    std::stringstream sstr;
+    sheet.set_column_names({"First","Last","Age","Major"});
+    sheet.add_row({"Amanda","Andrews","22","business"});
+    sheet.add_row({"Brian","Becker","21","computer science"});
+    sheet.add_row({"Carol","Conners","21","computer science"});
+    sheet.add_row({"Joe","Jackson","21","mathematics"});
+    sheet.add_row({"Sarah","Summers","21","computer science"});
+    sheet.add_row({"Diane","Dole","20","computer engineering"});
+    sheet.add_row({"David","Dole","22","electrical engineering"});
+    sheet.add_row({"Dominick","Dole","22","communications"});
+    sheet.add_row({"George","Genius","9","astrophysics"});
+    sheet.set_selection(new Select_And(
+		new Select_Contains(&sheet, "Major", "engineering"),
+		new Select_Contains(&sheet, "Last", "Dole")));
+	sheet.print_selection(sstr);
+	ASSERT_EQ("Diane Dole 20 computer engineering \nDavid Dole 22 electrical engineering \n", sstr.str());
 }
 
 #endif
